e4cb7533ad27e0ee66b71f7cf55265ca4a109a5d
x123 <x123@users.noreply.github.com>
2023-12-30 09:58:30 +0100

    add nix flake support

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description =
+    "A Nix-flake-based environment for slickmotd";
+
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.writeShellApplication {
+          name = "slickmotd";
+          runtimeInputs = with pkgs; [ bash figlet git ];
+          text = ./slickmotd.sh;
+        };
+      });
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            self.packages.${system}.default
+          ];
+        };
+      });
+
+    };
+}
